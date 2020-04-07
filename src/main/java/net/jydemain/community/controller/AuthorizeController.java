f0a78b1756bfae9d696a52a109cf83aa03ad2748
demain <jy9593z@163.com>
2020-04-07 16:37:44 +0800

    cookies和session的实现

--- src/main/java/net/jydemain/community/controller/AuthorizeController.java
+++ src/main/java/net/jydemain/community/controller/AuthorizeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Controller
@@ -27,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) throws IOException {
+                           @RequestParam(name = "state") String state,
+                            HttpServletRequest request) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -38,8 +40,14 @@ public class AuthorizeController {
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        if(user != null){
+            //登录成功，写cookie 和 session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
 
-        return "index";
     }
 }
